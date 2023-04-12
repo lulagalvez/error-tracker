e5061fd03a3563c059a779434e98fd98c6d23cce
cvenegasa <cvenegasa@udec.cl>
2023-04-12 13:11:27 -0400

    push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar';
+import './App.css';
+import React from 'react';
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+function App() {
+  return (
+      <>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+      </> 
+  );
+}
+
+export default App;
