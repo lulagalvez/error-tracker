4ed32ce4a8931e841c2fa24ff7d1905cca830ae2
cvenegasa <cvenegasa@udec.cl>
2023-04-12 17:22:25 -0400

    Se agregó un form de report bug 	modified:   src/components/Button.js 	new file:   src/components/ReportBug/bugForm.css 	new file:   src/components/pages/Contact.css 	new file:   src/components/pages/Home.js 	new file:   src/components/pages/Services.css Untracked files: 	src/components/ReportBug/bugForm.jsx 	src/components/pages/Contact.js 	src/components/pages/Services.js 	src/components/pages/crearBug.css 	src/components/pages/crearBug.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,6 +13,9 @@ export const Button= ({children, type, onClick, buttonStyle, buttonSize}) => {
     const checkButtonSize= SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return(
+        
+
+
         <Link to='/sign-up' className='btn-mobile'>
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}>
                 {children}
