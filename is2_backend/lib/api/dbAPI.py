352cc04de37b426c52f5840006867a9332fc153f
Nicolas Parra <Nispeter2@gmail.com>
2023-04-16 12:02:01 -0400

    updating api, adding dev & report

--- is2_backend/lib/api/dbAPI.py
+++ is2_backend/lib/api/dbAPI.py
@@ -57,7 +57,111 @@ def delete_user(id):
     return jsonify({'message': 'Usuario eliminado'})
 
 ####################################DEVELOPER###################################
+@app.route('/devs/<id>', methods=['GET'])
+def get_dev(id):
+    dev = Developer.query.get_or_404(id)
+    dev_data = {}
+    dev_data['id'] = dev.id
+    dev_data['name'] = dev.name
+    dev_data['email'] = dev.email
+    dev_data['role'] = dev.role
+    return jsonify({'dev': dev_data})
+
+@app.route('/devs', methods=['GET'])
+def get_devs():
+    devs = Developer.query.all()
+    temp = []
+    for dev in devs:
+        dev_data = {}
+        dev_data['id'] = dev.id
+        dev_data['name'] = dev.name
+        dev_data['email'] = dev.email
+        dev_data['role'] = dev.role
+        temp.append(dev_data)
+
+    return jsonify(temp)
+
+@app.route('/devs', methods=['POST'])
+def create_dev():
+    name = request.json['name']
+    email = request.json['email']
+    role = request.json["role"]
+    new_dev = Developer(name=name, email=email, role = role)
+    db.session.add(new_dev)
+    db.session.commit()
+    return jsonify({'message': 'Developer creado'})
+
+@app.route('/devs/<id>', methods=['PUT'])
+def update_dev(id):
+    dev = Developer.query.get_or_404(id)
+    name = request.json['name']
+    email = request.json['email']
+    role = request.json['role']
+    dev.name = name
+    dev.email = email
+    dev.role = role
+    db.session.commit()
+    return jsonify({'message': 'Developer actualizado'})
+
+@app.route('/devs/<id>', methods=['DELETE'])
+def delete_dev(id):
+    dev = Developer.query.get_or_404(id)
+    db.session.delete(dev)
+    db.session.commit()
+    return jsonify({'message': 'Developer eliminado'})
+
 #####################################REPORT#####################################
+@app.route('/reports/<id>', methods=['GET'])
+def get_report(id):
+    report = Report.query.get_or_404(id)
+    report_data = {}
+    report_data['id'] = report.id
+    report_data['title'] = report.title
+    report_data['description'] = report.description
+    return jsonify({'report': report_data})
+
+@app.route('/reports', methods=['GET'])
+def get_reports():
+    reports = Report.query.all()
+    temp = []
+    for report in reports:
+        report_data = {}
+        report_data['id'] = report.id
+        report_data['title'] = report.title
+        report_data['description'] = report.description
+        temp.append(report_data)
+
+    return jsonify(temp)
+
+@app.route('/reports', methods=['POST'])
+def create_report():
+    title = request.json['title']
+    description = request.json['desctription']
+    date = request.json["date"]
+    new_report = Report(title=title, description=description, date = date)
+    db.session.add(new_report)
+    db.session.commit()
+    return jsonify({'message': 'Reporte creado'})
+
+@app.route('/reports/<id>', methods=['PUT'])
+def update_report(id):
+    report = Report.query.get_or_404(id)
+    title = request.json['title']
+    description = request.json['desctription']
+    date = request.json["date"]
+    report.title =  title
+    report.description = description
+    report.date = date
+    db.session.commit()
+    return jsonify({'message': 'Reporte actualizado'})
+
+@app.route('/reports/<id>', methods=['DELETE'])
+def delete_report(id):
+    report = Report.query.get_or_404(id)
+    db.session.delete(report)
+    db.session.commit()
+    return jsonify({'message': 'Reporte eliminado'})
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
