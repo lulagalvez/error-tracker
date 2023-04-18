8599c5f71e4c086189d5a20b3bd36be1fa0e59f6
cvenegasa <cvenegasa@udec.cl>
2023-04-17 20:03:44 -0400

    TODO: arreglar el undefined de bugModel en bugForm Changes to be committed: modified:   is2_frontend/src/components/APIService.js modified:   is2_frontend/src/components/ReportBug/bugForm.jsx modified:   is2_frontend/src/models/bugModel.js

--- is2_frontend/src/components/ReportBug/bugForm.jsx
+++ is2_frontend/src/components/ReportBug/bugForm.jsx
@@ -3,18 +3,26 @@ import './bugForm.css'
 import BugModel from '../../models/bugModel';
 import APIService from '../APIService';
 //TODO: investigar acerca de los useState hooks y APIService para futura conexión con Flask API 
+//TODO: arreglar bugModel para que no quede undefined 
 /*
 
 */
 
 export default (props) =>{
-    const[bugObject,setBugObject]=useState[new BugModel(props.bug)];
+    const [bugObject,setBugObject]=useState(new BugModel(props.bug))
 
-    
+
     const reportBug= () =>{
-        APIService.ReportBug(bugObject.titulo,bugObject.descripcion,Date.now())
-        .then((response) => props.insertedArticle(response))
+        APIService.post('reports',{title: bugObject?.titulo, description: bugObject?.descripcion,date:Date.now()})
+        .then((response) => props.insertedReport(response))
         .catch(error => console.log('error',error))
+    }
+
+    function inputChanged(e){
+        setBugObject({
+            ...bugObject,
+            [e.target.name]:e.target.value
+        })
     }
 
     const handleSubmit=(e)=>{
@@ -22,8 +30,7 @@ export default (props) =>{
         setBugObject({
             ...bugObject,
             [e.target.name]:e.target.value
-            
-        })
+        });
         reportBug();
 
     }
@@ -33,21 +40,22 @@ export default (props) =>{
     return(
         <div className='crear-bug'>
             <h1 className='titulo'>{props.title}</h1>
+
             <form onSubmit={handleSubmit}>
                 <label>Título: </label>
                 <input name='titulo' placeholder='Titulo del ticket' maxLength={30} required
-                 onChange={handleSubmit} value={bugObject.titulo} ></input> 
+                 onChange={inputChanged} value={bugObject?.titulo} ></input> 
                 <label> Software: </label>
-                <select name='software' required onChange={handleSubmit} value={bugObject.software} > 
+                <select name='software' required onChange={inputChanged} value={bugObject?.software} > 
                     <option value='1'>Software 1</option>
                     <option value='2'>Software 2</option>
                 </select>
                 <label> Descripción:</label>
                 <textarea name='detalles' placeholder='Descripción detallada del bug' required 
-                onChange={handleSubmit} value={bugObject.detalles} ></textarea>
+                onChange={inputChanged} value={bugObject?.detalles} ></textarea>
                 <label> Pasos: </label>
                 <textarea name='pasos' placeholder='Pasos detallados para reproducir el bug' required
-                onChange={handleSubmit} value={bugObject.pasos}  ></textarea>
+                onChange={inputChanged} value={bugObject?.pasos}  ></textarea>
                 <style>{` .red {color: red}     .green {color: green}`}</style>
                 <button type='submit'> <a>{props.title}</a></button>
             </form>
