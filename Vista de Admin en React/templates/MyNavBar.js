c50a61143d2ac7c9093b030f97c388d9666dc0db
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2023-05-25 15:38:26 -0400

    Create MyNavBar.js

---
+++ Vista de Admin en React/templates/MyNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MyNavBar = () => {
+  return (
+    <div>
+        <nav class="navbar bg-dark" data-bs-theme="dark">
+          <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Vista de admin</span>
+          </div>
+        </nav>
+    </div>
+  )
+}
