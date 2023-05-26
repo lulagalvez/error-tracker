0d4f2422ba09a6b78cfc8ddb900d108d642a05ab
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2023-05-26 13:43:57 -0400

    Create BotonBorrar.js

---
+++ vista_admin_react/src/BotonBorrar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BotonBorrar = () => {
+  return (
+    <button type="button" className="btn btn-danger">
+        <i class="bi bi-trash3"></i>
+    </button>
+  )
+}
