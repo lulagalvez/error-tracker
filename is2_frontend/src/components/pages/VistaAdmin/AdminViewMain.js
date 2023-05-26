c51cd8140cd9b7ea7fc05b616ecacebd5384e601
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2023-05-26 19:56:21 -0400

    Create AdminViewMain.js

---
+++ is2_frontend/src/components/pages/VistaAdmin/AdminViewMain.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import {MyNavBar} from './templates/MyNavBar';
+import {TablaAdmin} from './templates/TablaAdmin';
+/*import {CambiarEstado} from './templates/CambiarEstado'
+import {CambiarPrioridad} from './templates/CambiarPrioridad'*/
+
+function AdminViewMain() {
+  return (
+    <div>
+      <React.Fragment>
+        <br/><br/>
+        <TablaAdmin/>
+        <br/><br/>
+      </React.Fragment>
+    </div>
+  );
+}
+
+export default AdminViewMain;
