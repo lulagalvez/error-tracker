1e1589f196d4a694308599f7b5f8f2d3c83d7e03
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2023-05-29 19:08:05 -0400

    Update BotonBorrar.js

--- is2_frontend/src/components/pages/AdminView/BotonBorrar.js
+++ is2_frontend/src/components/pages/AdminView/BotonBorrar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './BotonBorrar.css'
 
-export const BotonBorrar = () => {
+export const BotonBorrar = ({deleteFunction}) => {
   return (
-    <button type="button" className="btn bg-transparent">
+    <button type="button" className="btn bg-transparent" onClick={deleteFunction}>
         <i class="bi bi-trash3 color-trash"></i>
     </button>
   )
