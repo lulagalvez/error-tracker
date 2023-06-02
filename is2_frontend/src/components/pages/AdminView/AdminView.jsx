b0408b3d083a9c2320614086c76759bf5e194f32
cvenegasa <cvenegasa@udec.cl>
2023-06-01 22:44:29 -0400

    cleaning

--- is2_frontend/src/components/pages/AdminView/AdminView.jsx
+++ is2_frontend/src/components/pages/AdminView/AdminView.jsx
@@ -2,12 +2,8 @@ import React,{useEffect, useState} from 'react'
 import APIService from '../../services/APIService';
 import {BotonBorrar} from './BotonBorrar'
 import {Buscador} from './Buscador'
-import {CambiarPrioridad} from './CambiarPrioridad'
-import {CambiarEstado} from './CambiarEstado'
 import {Status} from './Status'
 import './TablaAdmin.css'
-import '../Sidebars/SidebarAdmin'
-import SideBarAdmin from '../Sidebars/SidebarAdmin';
 
 function AdminView() {
     const [reports,setReports] =React.useState([])
@@ -31,23 +27,6 @@ function AdminView() {
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-
-        // apiservice.get('reports')
-        // //.then(response => response.json())
-        // .then(response => {
-        //     console.log('reports',response);
-        //     setReports(response);
-        // })
-        //  apiservice.get('devs')
-        // .then(response =>{
-        //     console.log('devs',response);
-        //     setDevs(response);
-        // })
-        // // devsName=  devs.map(item=> ({label: item.name, value: item.name == 'N/A' ? 'queso' : item.id }))
-        // // devsName = devs.map(function (item){
-        // //     {label: item.name, value: item.name == 'N/A' ? 'queso' : item.id }
-        // // } )
-        // console.log('devs name', devs.map(item=> ({label: item.name, value: item.name == 'N/A' ? 'queso' : item.id })))
     }
     
 
@@ -71,14 +50,13 @@ function AdminView() {
         dato.title.toLowerCase().includes(search.toLocaleLowerCase())
         )
     }
-    const [num,setNum]=useState(0);
+    const [numP,setNumP]=useState(0);
     const [idP,setIdP]=useState(0);
+    const [numE,setNumE]=useState(0);
     const [idE,setIdE]=useState("");
     const [estado,setEstado]=useState("");
-    //const [results,setBugs]=useState(results);
     useEffect( ()=> {
         showData()
-      //  console.log('bugs', bugs);
     }, [])
 
     // async function handleDevSelect(event) {
@@ -88,45 +66,20 @@ function AdminView() {
     //     // setReports(reportsResponse);
     // }
 
-    const cambiarIdE=(nuevo)=>{
-      setIdE(nuevo);
-    }
-    const cambiarEstado=(nuevo)=>{
-      setEstado(nuevo);
-    }
-    /*const copiaArray=()=>{
-        var newArray=[];
-        results.map((bug)=>{
-            if(bug!==undefined) newArray.push(bug);
-        })
-        return newArray;
-    }*/
     const getPos=(bug_id,array)=>{
         for(var i=0;i<array.length;i++){
             if(bug_id.startsWith(array[i].id)) return i;
         }
         return -1;
     }
-    const setPrioridad=()=>{
-     /*    //var newArray=copiaArray();
-        const pos=getPos(bug_id,reports);
-        if(pos==-1) return;
-        reports[pos].urgency=nuevaPr;
-        reports.sort((a,b)=>{
-            return a.urgency-b.urgency;
-        });
-       // setBugs(newArray); */
-       
-       apiservice.patch('reports',num,idP,'priority')
+    const setPrioridad=(event)=>{
+
+       apiservice.patch('reports',numP,idP,'priority')
        .then(response => console.log(response)); 
     }
-    const setStatus=(bug_id,nuevoSt)=>{
-        if(nuevoSt=="") return;
-        //var newArray=copiaArray();
-        const pos=getPos(bug_id,reports);
-        if(pos==-1) return;
-        reports[pos].state=nuevoSt;
-        //setBugs(newArray);
+    const setStatus=(event)=>{
+        apiservice.patch('reports',numE,idE,'status')
+       .then(response => console.log(response)); 
     }
     const setDev=(pos,nuevoDev)=>{
         //var newArray=copiaArray();
@@ -156,16 +109,25 @@ function AdminView() {
             <div>
                 <p>
                     Cambiar prioridad: -
-                    <input type="text" onChange={e => setNum(e.target.value)} placeholder="ID"/>
+                    <input type="text" onChange={e => setNumP(e.target.value)} placeholder="ID"/>
                     <input type="number" onChange={e => setIdP(e.target.value)} placeholder="Prioridad"/>
-                    <button onClick={setPrioridad()}>Cambiar</button>
+                    <button onClick={setPrioridad}>Cambiar</button>
                 </p>
             </div>
-            <CambiarEstado
-                cambiarId={nuevo => cambiarIdE(nuevo)}
-                cambiarStatus={nuevo => cambiarEstado(nuevo)}
-                clickFunction={e=>setStatus(idE,estado)}
-            />
+            <div>
+      <p>
+            Cambiar estado: -
+            <input type="text" onChange={e => setNumE(e.target.value)} placeholder='ID'/>
+            <select onChange={e => setIdE(e.target.value)}>
+                <option value="">--Seleccione una opcion--</option>
+                <option value="Closed">Cerrado</option>
+                <option value="Pending">Pendiente</option>
+                <option value="ToDo">Por asignar</option>
+                <option value="Testing">Testing</option>
+            </select>
+            <button onClick={setStatus}>Cambiar</button>
+      </p>
+    </div>
         </div>
         <table class="table table-striped">
         <thead>
@@ -184,13 +146,13 @@ function AdminView() {
             {results.map((val, key) => {
                 return (
                     <tr key={key}>
-                        <td>{val.id}</td>
-                        <td>{val.title}</td>
-                        <td>{val.user_name}</td>
-                        <td>{val.software}</td>
-                        <td>{val.date}</td>
+                        <td style={{ textAlign: 'center' }}>{val.id}</td>
+                        <td style={{ textAlign: 'center' }}>{val.title}</td>
+                        <td style={{ textAlign: 'center' }}>{val.user_name}</td>
+                        <td style={{ textAlign: 'center' }}>{val.software_name}</td>
+                        <td style={{ textAlign: 'center' }}>{val.date}</td>
                         {/* <td>{val.description}</td> */}
-                        <td>{val.urgency}</td>
+                        <td style={{ textAlign: 'center' }}>{val.urgency}</td>
                         <td><Buscador
                             texto={val.dev_id}
                             setTexto={nuevo => setDev(key,nuevo)}
@@ -207,11 +169,10 @@ function AdminView() {
                        
                         <div className='Devs-container' style={{ width: '300px' }}>
                         <p>{selectedDev}</p>
-
                         <select id="devs" onChange={handleDevSelect}>
                             <option value="">--Please choose an option--</option>
                             {devs.map(dev => (
-                            <option key={dev.id} value={dev.id}>
+                            <option key={dev.name} value={dev.name}>
                                 {dev.name}
                             </option>
                             ))}
