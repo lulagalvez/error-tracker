d35c3f00833684d968b17ca6a68718933e64056d
pvzlover25 <105994379+pvzlover25@users.noreply.github.com>
2023-06-22 15:34:44 -0400

    Traduccion de Fecha y Prioridad

--- is2_frontend/src/components/pages/AdminView/TicketRow.jsx
+++ is2_frontend/src/components/pages/AdminView/TicketRow.jsx
@@ -6,7 +6,17 @@ import TicketExpansion from "./TicketExpansion";
 const TicketRow = ({ ticket, handleClick, deleteReport, selectedTicket }) => {
   const isTicketSelected = selectedTicket === ticket;
   const rowClassName = isTicketSelected ? "active" : "";
-
+  const formatoFecha=(strFecha)=>{
+    const ret = new Date(strFecha);
+    return ret.toLocaleDateString();
+  }
+  const formatoUrg=(strUrg)=>{
+    const numUrg=Number(strUrg);
+    if(numUrg==1) return "Urgente";
+    if(numUrg>=2 && numUrg<=4) return "Alta";
+    if(numUrg>=5 && numUrg<=7) return "Media";
+    return "Baja";
+  }
   return (
     <>
       <tr onClick={() => handleClick(ticket)} className={rowClassName}>
@@ -14,8 +24,8 @@ const TicketRow = ({ ticket, handleClick, deleteReport, selectedTicket }) => {
         <td style={{ textAlign: "center" }}>{ticket.title}</td>
         <td style={{ textAlign: "center" }}>{ticket.user_name}</td>
         <td style={{ textAlign: "center" }}>{ticket.software_name}</td>
-        <td style={{ textAlign: "center" }}>{ticket.date}</td>
-        <td style={{ textAlign: "center" }}>{ticket.urgency}</td>
+        <td style={{ textAlign: "center" }}>{formatoFecha(ticket.date)}</td>
+        <td style={{ textAlign: "center" }}>{formatoFecha(ticket.urgency)}</td>
         <td style={{ textAlign: "center" }}>{ticket.dev_name}</td>
         <td>
           <Status nombre={ticket.status} />
